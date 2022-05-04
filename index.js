e457353828641572a3ef8194302cc22f3efe5719
TimMcMullan <mcmullant@gmail.com>
2022-05-04 18:22:50 -0400

    test

--- index.js
+++ index.js
@@ -8,36 +8,56 @@ const promptuser = () => {
             type: 'list',
             name: 'choice',
             message: 'What would you like to do?',
-            choices: [{ 
+            choices: [{
+                name: 'View All Departments',
+                value: 'VIEW_ALL_DEPARTMENTS'
+            }, 
+                {
+                name: 'View All Roles',
+                value: 'VIEW_ALL_ROLES'
+            }, 
+                { 
                 name: 'View All Employees',
                 value: 'VIEW_ALL_EMPLOYEES'
             }, 
             {
-                name: 'Add Employee',
-                value: 'ADD_EMPLOYEE'
-            }, 
-            {
-                name: 'View All Roles',
-                value: 'VIEW_ALL_ROLES'
-            }, 
+                name: 'Add Department',
+                value: 'ADD_DEPARTMENT'
+            },
             {
                 name: 'Add Role',
                 value: 'ADD_ROLE'
-            }, 
+            },             
             {
-                name: 'View All Departments',
-                value: 'VIEW_ALL_DEPARTMENTS'
-            }, 
+                name: 'Add Employee',
+                value: 'ADD_EMPLOYEE'
+            },
             {
-                name: 'Add Department',
-                value: 'ADD_DEPARTMENT'
+                name: 'Update Employee',
+                value: 'UPDATE_EMPLOYEE'
             }],
         },
     ]).then(res => {
         let choice = res.choice;
         switch(choice) {
+            case "VIEW_ALL_DEPARTMENTS": 
+            db.viewDepartments().then(([rows]) => {
+                let employees = rows;
+                console.log("\n");
+                console.table(employees);
+              })
+              .then(() => promptuser());
+            break;
+            case "VIEW_ALL_ROLES": 
+            db.viewRoles().then(([rows]) => {
+                let employees = rows;
+                console.log("\n");
+                console.table(employees);
+              })
+              .then(() => promptuser());
+            break;            
             case "VIEW_ALL_EMPLOYEES": 
-            db.findEmployees().then(([rows]) => {
+            db.viewEmployees().then(([rows]) => {
                 let employees = rows;
                 console.log("\n");
                 console.table(employees);
@@ -51,9 +71,18 @@ const promptuser = () => {
                 console.table(employees);
               })
               .then(() => promptuser());;
-            db.removeEmployee();
+            db.viewRole();
             break;
-            case "VIEW_ALL_EMPLOYEES": 
+            case "**#VIEW_ALL_ROLES1": 
+            db.viewRoles().then(([rows]) => {
+                let employees = rows;
+                console.log("\n");
+                console.table(employees);
+              })
+              .then(() => promptuser());;
+            db.findEmployees();
+            break;
+            case "VIEW_ALL_EMPLOYEES1": 
             db.XXXXXXXXX().then(([rows]) => {
                 let employees = rows;
                 console.log("\n");
@@ -62,33 +91,8 @@ const promptuser = () => {
               .then(() => promptuser());;
             db.findEmployees();
             break;
-            case "VIEW_ALL_EMPLOYEES": 
-            db.XXXXXXXXX().then(([rows]) => {
-                let employees = rows;
-                console.log("\n");
-                console.table(employees);
-              })
-              .then(() => promptuser());;
-            db.findEmployees();
-            break;
-            case "VIEW_ALL_EMPLOYEES": 
-            db.XXXXXXXXX().then(([rows]) => {
-                let employees = rows;
-                console.log("\n");
-                console.table(employees);
-              })
-              .then(() => promptuser());;
-            db.findEmployees();
-            break;
-            case "VIEW_ALL_EMPLOYEES": 
-            db.XXXXXXXXX().then(([rows]) => {
-                let employees = rows;
-                console.log("\n");
-                console.table(employees);
-              })
-              .then(() => promptuser());;
-            db.findEmployees();
-            break;
+
+
         }
     })
   
