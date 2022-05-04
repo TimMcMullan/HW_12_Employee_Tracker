e3377abbef1fecc845b106256ee21f22bad43d59
TimMcMullan <mcmullant@gmail.com>
2022-05-03 22:10:30 -0400

    nothing is working

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 // Import and require mysql2
 const mysql = require('mysql2');
 const inquirer = require("inquirer");
+const res = require('express/lib/response');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -22,6 +23,7 @@ const db = mysql.createConnection(
     console.log(`Connected to the employees_db database.`)
 );
 
+
 console.log("What would you like to do?");
 
 const promptuser = () => {
@@ -30,24 +32,59 @@ const promptuser = () => {
             type: 'list',
             message: 'What would you like to do?',
             choices: ['View All Employees', 'Add Employee', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department'],
-        }
+            name: 'userInput'
+        },
     ])
-}
+        .then(() => {
+            if (data = "View All Employees") {
+                console.log("This is the if statement");
+            } else if 
+                (data = "Add Employee") {
+                    console.log = ("Add Employee statement");
+                } else {
+                    console.log = ("out of options");
+                };
+            });
+};
+
+            //         else if (userInput != "View All Employees") {
+            //             res.json({
+            //                 message: 'Option not available'
+            //             });
+            //           }  else {
+            //                 allEmployees();
+            //             };
+            //         }
+            //     );
+            // };
 
 
+//             someVar = [];
+
+//             var allEmployees = function () {
+//                 db.query(`SELECT *
+// FROM employee e
+// LEFT JOIN role r ON (e.erole_id = r.role_id)
+// LEFT JOIN department d ON (d.dept_id = r.department_id)`, function (err, results) {
+//                     allEmployeesOutput(results);
+//                 })
+//             };
+//             function allEmployeesOutput(value) {
+//                 someVar = value;
+//                 // console.log(someVar[0]);
+//                 console.log("it worked!");
+//                 return;
+//             };
 
 
-someVar = [];
-
-let allEmployees = db.query(`SELECT *
-FROM employee e
-LEFT JOIN role r ON (e.erole_id = r.role_id)
-LEFT JOIN department d ON (d.dept_id = r.department_id)`, function (err, results) {
-    allEmployeesOutput(results);
-});
-function allEmployeesOutput(value) {
-    someVar = value;
-    console.log(someVar);
-    return;
-}
-// console.log(someVar);
+            const init = () => {
+                promptuser()
+                    // .then((answers) => {
+                    .then((userInput) => {
+                        console.log("function invoked");
+                        // answer.userInput = "View All Employees";
+                        // allEmployees();
+                    }
+                    );
+                };
+            init();
